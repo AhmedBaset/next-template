f69182f52a29cf89b864dfb4e5913f7845ea0595
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2023-06-30 01:47:33 +0300

    feat: Add type safty and auto complete for `.env` vars

---
+++ src/types/index.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+namespace NodeJS {
+	interface ProcessEnv {
+		NODE_ENV: "development" | "production" | "test";
+	}
+}
