d6fdd35d6791e4aa7ca031b3f8dbfc2128364ddd
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-08-17 22:16:51 +0000

    feat: absoluteUrl util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function absoluteUrl(path: `/${string}`) {
+  // TODO
+  // if (env.NEXT_PUBLIC_APP_URL) return `${env.NEXT_PUBLIC_APP_URL}${path}`
+  return `http://localhost:3000${path}`
+}
