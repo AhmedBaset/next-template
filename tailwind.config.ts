60a0f055194cc89bb7b00da545520f7f65f99f28
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-08-22 15:15:55 +0000

    note about tailwindcss-animate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,20 @@
 import { Config } from "tailwindcss";
 
-import { default as shadcnPlugin } from "./src/lib/shadcnPlugin";
+import { shadcnPlugin } from "./src/lib/shadcn-plugin";
+
+// import { tailwindAnimate } from "tailwindcss-animate";
+
+/**
+ * We use require() here because tailwindcss-animate doesn't have a declaration file.
+ * @see https://github.com/jamiebuilds/tailwindcss-animate/issues/44
+ * TODO: Remove this when the issue is resolved.
+ */
+const tailwindAnimate = require("tailwindcss-animate");
 
 const config = {
-	darkMode: ["class"],
-	content: ["./src/**/*.{ts,tsx}"],
-	plugins: [
-		require("tailwindcss-animate"),
-		shadcnPlugin,
-	],
+  darkMode: ["class"],
+  content: ["./src/**/*.{ts,tsx}"],
+  plugins: [tailwindAnimate, shadcnPlugin],
 } satisfies Config;
 
 export default config;
