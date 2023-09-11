a420b9dcbcdbf0dc57912786d87f6f23a48d1191
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-09-11 14:32:52 +0000

    refactor: use esm of `tailwindcss-animate`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,7 @@
 import { Config } from "tailwindcss";
+import tailwindAnimate from "tailwindcss-animate";
 
 import { shadcnPlugin } from "./src/lib/shadcn-plugin";
-
-// import { tailwindAnimate } from "tailwindcss-animate";
-
-/**
- * We use require() here because tailwindcss-animate doesn't have a declaration file.
- * @see https://github.com/jamiebuilds/tailwindcss-animate/issues/44
- * TODO: Remove this when the issue is resolved.
- */
-const tailwindAnimate = require("tailwindcss-animate");
 
 const config = {
   darkMode: ["class"],
