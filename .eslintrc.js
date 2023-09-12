cdd2bd99ac4d2282b1168c1c88fea3648b9f1584
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-09-12 13:21:58 +0000

    chore: formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,67 +1,67 @@
 module.exports = {
-	$schema: "https://json.schemastore.org/eslintrc",
-	root: true,
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		"next/core-web-vitals",
-		"prettier",
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:tailwindcss/recommended",
-		"plugin:prettier/recommended"
-	],
-	overrides: [
-		{
-			files: ["*.ts", "*.tsx"],
-			parser: "@typescript-eslint/parser",
-		},
-	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
-	},
-	plugins: ["react", "@typescript-eslint", "tailwindcss", "prettier"],
-	rules: {
-		"prettier/prettier": "warn",
-		indent: "off", // handled by prettier
-		quotes: ["warn", "double"],
-		semi: "warn",
-		"no-console": "warn",
-		"tailwindcss/no-custom-classname": "off",
-		"react/react-in-jsx-scope": "off",
-		"react/prop-types": "off",
-		"no-unused-vars": "off",
-		"@typescript-eslint/no-unused-vars": [
-			"warn",
-			{
-				argsIgnorePattern: "^_",
-				varsIgnorePattern: "^_",
-				caughtErrorsIgnorePattern: "^_",
-			},
-		],
-		"no-mixed-spaces-and-tabs": "off",
-	},
-	settings: {
-		tailwindcss: {
-			callees: ["cn"],
-			config: "tailwind.config.ts",
-		},
-		next: {
-			rootDir: ["./"],
-		},
-	},
-	ignorePatterns: [
-		"node_modules/",
-		".next/",
-		"out/",
-		"public/",
-		"dist/",
-		".cache/",
-	],
+  $schema: "https://json.schemastore.org/eslintrc",
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ["react", "@typescript-eslint", "tailwindcss", "prettier"],
+  extends: [
+    "next/core-web-vitals",
+    "prettier",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:tailwindcss/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
+  rules: {
+    "prettier/prettier": "warn",
+    indent: "off", // handled by prettier
+    quotes: ["warn", "double"],
+    semi: "warn",
+    "no-console": "warn",
+    "tailwindcss/no-custom-classname": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "no-mixed-spaces-and-tabs": "off",
+  },
+  settings: {
+    tailwindcss: {
+      callees: ["cn"],
+      config: "tailwind.config.ts",
+    },
+    next: {
+      rootDir: ["./"],
+    },
+  },
+  ignorePatterns: [
+    "node_modules/",
+    ".next/",
+    "out/",
+    "public/",
+    "dist/",
+    ".cache/",
+  ],
 };
